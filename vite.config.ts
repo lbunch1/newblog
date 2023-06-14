6425135c0e31455dc0fc7e2de24409cbd238dd44
Logan Bunch <loganbunch@gmail.com>
2023-06-14 11:56:07 -0700

    vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
+    solid({ adpater: vercel() }),
     {
       ...(await import("@mdx-js/rollup")).default({
         jsx: true,
